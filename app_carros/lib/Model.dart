4613ffec44ea023f4b30cfa7eaf11162c02ea8bb
oliveiramarcelo12 <oliveiramarcelo198@gmail.com>
2024-02-29 19:51:38 -0300

    29/09

---
+++ app_carros/lib/Model.dart
@@ -0,0 +1,23 @@
+
+class Carro{
+  String _modelo;
+  int _ano;
+  String _imagemUrl;
+  Carro(this._modelo,this._ano,this._imagemUrl);
+  String get modelo => _modelo;
+  int get ano => _ano;
+  String get imageUrl => _imagemUrl;
+  set modelo(String novoModelo){
+    if (novoModelo.isNotEmpty) {
+      _modelo = novoModelo;
+      
+    }
+  }
+   set ano(String novoAno){
+    if (novoAno >= 1900) {
+      _ano = novoAno;
+      
+    }
+  }
+
+}
