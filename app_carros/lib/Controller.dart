565d5846e8dec7982ae4d7a2f73839c262bd9abb
oliveiramarcelo12 <oliveiramarcelo198@gmail.com>
2024-02-29 21:43:20 -0300

    29/02

--- app_carros/lib/Controller.dart
+++ app_carros/lib/Controller.dart
@@ -1,8 +1,12 @@
 import 'package:app_carros/Model.dart';
 
 class CarroController{
-  List<Carro> _carroLista = [];
+  List<Carro> _carroLista = [
+     Carro("Fiat Uno",1992,"caminho da imagem"),
+     Carro("Classic",2012,"caminho da imagem")
 
+  ];
+ 
   List<Carro> get listarCarros => _carroLista;
 
   void adicionarCarro(String modelo, int ano, String imagemUrl){
