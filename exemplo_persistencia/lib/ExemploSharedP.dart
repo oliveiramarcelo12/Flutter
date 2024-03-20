a2f4e0a5aecdfae661af4fd9203fcef15a31aeea
oliveiramarcelo12 <oliveiramarcelo198@gmail.com>
2024-03-19 21:48:53 -0300

    19/03

--- exemplo_persistencia/lib/ExemploSharedP.dart
+++ exemplo_persistencia/lib/ExemploSharedP.dart
@@ -1,24 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
-    title: "Lista de Tarefas",
-    theme: ThemeData(primarySwatch: Colors.blue),
-    home: TaskListScreen(),
-   )
+    return MaterialApp(
+      title: "Lista de Tarefas",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: TaskListScreen(),
+    );
   }
-
-  
 }
 
 class TaskListScreen extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-  
+  State<StatefulWidget> createState() => _TaskListScreenState();
+}
+
+class _TaskListScreenState extends State<TaskListScreen> {
+  List<String> tasks = []; // Lista de tarefas
+  final TextEditingController _controller =
+      TextEditingController(); // Controlador de texto para o campo de entrada de nova tarefa
+
+  @override
+  void initState() {
+    super.initState();
+    loadTasks(); // Carrega as tarefas ao iniciar a tela
+  }
+
+  Future<void> loadTasks() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      tasks = prefs.getStringList('tasks') ?? [];
+    });
+  }
+
+  Future<void> saveTasks() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList('tasks', tasks);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de Tarefas'),
+      ),
+      body: ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(tasks[index]),
+            onLongPress: () {
+              setState(() {
+                tasks.removeAt(index);
+                saveTasks();
+              });
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text('Nova Tarefa'),
+                content: TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    hintText: 'Digite a tarefa',
+                  ),
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        tasks.add(_controller.text);
+                        saveTasks();
+                        _controller.clear();
+                        Navigator.of(context).pop();
+                      });
+                    },
+                    child: Text('Adicionar'),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
