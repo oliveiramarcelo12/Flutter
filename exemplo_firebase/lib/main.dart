0072772192044c4837baafee2d2f6ed573896780
oliveiramarcelo12 <oliveiramarcelo198@gmail.com>
2024-06-04 21:06:04 -0300

    04/06

---
+++ exemplo_firebase/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:exemplo_firebase/firebase_options.dart';
+import 'package:exemplo_firebase/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
